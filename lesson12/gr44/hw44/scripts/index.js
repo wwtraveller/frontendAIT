3e6935a96b7f17d81faee757066a296ce00d0eca
Alena Shilimova <ashilimova@mail.ru>
2024-04-18 00:56:09 +0200

    add imt function and switch

--- lesson12/gr44/hw44/scripts/index.js
+++ lesson12/gr44/hw44/scripts/index.js
@@ -7,8 +7,29 @@ const resultElement = document.querySelector('#result');
 
 getResultButton.addEventListener('click', showResult);
 
+// для человека с весом 73 кг и ростом 172 см: ИМТ = 73 : (1,72 х 1,72) = 24,68 кг/м2.
 function showResult(){
     let resultOfAdding;
-    resultOfAdding = +heightInput.value + +weightInput.value;
+    resultOfAdding = Math.round(+weightInput.value / Math.pow((+heightInput.value/100),2));
     resultImt.innerText = resultOfAdding;
 }
+
+const getImtDescription = function(imt, description){
+    return description (imt)
+}
+const description = function(imt){
+    switch (imt) {
+        case "ИМТ < 18": return "Ниже нормального веса";
+        case "ИМТ >= 19 И < 25": return "Нормальный вес";
+        case "ИМТ >= 25 И < 30": return "Избыточный вес";
+        case "ИМТ >= 30 И < 35": return "Ожирение I степени";
+        case "ИМТ >= 35 И < 40": return "Ожирение II степени";
+        case "ИМТ >= 40": return "Ожирение III степени";
+        default: return "no such imt";
+    }
+}
+const res1 = getImtDescription("ИМТ >= 19 И < 25", description);
+console.log(res1);
+// console.log(getImtDescription("ИМТ < 18", description));
+// console.log(getImtDescription("ИМТ < 18", description));
+// console.log(description("ИМТ < 18"));
